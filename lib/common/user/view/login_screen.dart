c135d1eb846434a85aeb6b6333a1994433cc2628
goback <goback0317@yahoo.co.jp>
2022-09-22 12:23:50 +0900

    UI 마무리하기

--- lib/common/user/view/login_screen.dart
+++ lib/common/user/view/login_screen.dart
@@ -9,46 +9,52 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      body: SafeArea(
-        top: true,
-        bottom: false,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              _Title(),
-              _SubTitle(),
-              Image.asset(
-                'asset/img/misc/logo.png',
-                width: MediaQuery.of(context).size.width / 3 * 2,
-              ),
-              CustomTextFormField(
-                hintText: '이메일을 입력해주세요.',
-                onChanged: (value) {},
-              ),
-              CustomTextFormField(
-                hintText: '비밀번호 입력해주세요.',
-                obscureText: true,
-                onChanged: (value) {},
-              ),
-              ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: PRIMARY_COLOR,
+      body: SingleChildScrollView(
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: SafeArea(
+          top: true,
+          bottom: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _Title(),
+                const SizedBox(height: 16.0),
+                _SubTitle(),
+                Image.asset(
+                  'asset/img/misc/logo.png',
+                  width: MediaQuery.of(context).size.width / 3 * 2,
                 ),
-                child: Text(
-                  '로그인',
+                CustomTextFormField(
+                  hintText: '이메일을 입력해주세요.',
+                  onChanged: (value) {},
                 ),
-              ),
-              TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(
-                  foregroundColor: Colors.black,
+                const SizedBox(height: 16.0),
+                CustomTextFormField(
+                  hintText: '비밀번호 입력해주세요.',
+                  obscureText: true,
+                  onChanged: (value) {},
                 ),
-                child: Text('회원가입'),
-              ),
-            ],
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: PRIMARY_COLOR,
+                  ),
+                  child: Text(
+                    '로그인',
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                  ),
+                  child: Text('회원가입'),
+                ),
+              ],
+            ),
           ),
         ),
       ),
