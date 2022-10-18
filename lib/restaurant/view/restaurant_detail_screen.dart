b12a2a7125aa7d309399c8b7aac204b6f87b4d8a
goback <goback0317@yahoo.co.jp>
2022-10-18 14:52:28 +0900

    SliverList 구현하기

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -6,27 +6,63 @@ import 'package:prac/restaurant/component/restaurant_card.dart';
 class RestaurantDetailScreen extends StatelessWidget {
   const RestaurantDetailScreen({Key? key}) : super(key: key);
 
+  SliverToBoxAdapter renderTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
+        name: '불타는 떡볶이',
+        tags: ['떡볶이', '맛있음', '치즈'],
+        ratingsCount: 100,
+        deliveryTime: 30,
+        deliveryFee: 3000,
+        ratings: 4.76,
+        isDetail: true,
+        detail: '맛있는 떡볶이',
+      ),
+    );
+  }
+
+  SliverPadding renderProducts() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          childCount: 10,
+          (context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: ProductCard(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  renderLabel() {
+    return SliverPadding(
+      padding: EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverToBoxAdapter(
+        child: Text(
+          '메뉴',
+          style: TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
       title: '불타는 떡볶이',
-      body: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset('asset/img/food/ddeok_bok_gi.jpg'),
-            name: '불타는 떡볶이',
-            tags: ['떡볶이', '맛있음', '치즈'],
-            ratingsCount: 100,
-            deliveryTime: 30,
-            deliveryFee: 3000,
-            ratings: 4.76,
-            isDetail: true,
-            detail: '맛있는 떡볶이',
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            child: ProductCard(),
-          ),
+      body: CustomScrollView(
+        slivers: [
+          renderTop(),
+          renderLabel(),
+          renderProducts(),
         ],
       ),
     );
