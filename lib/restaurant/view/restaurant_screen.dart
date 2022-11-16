e8669ec5da626879e7776b27cadeb504e95ed43a
goback <goback0317@yahoo.co.jp>
2022-11-16 12:15:08 +0900

    완성된 Pagination 로직 실행해보기

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -5,11 +5,32 @@ import 'package:prac/restaurant/component/restaurant_card.dart';
 import 'package:prac/restaurant/view/restaurant_detail_screen.dart';
 import 'package:prac/restaurant/provider/restaurant_provider.dart';
 
-class RestaurantScreen extends ConsumerWidget {
+class RestaurantScreen extends ConsumerStatefulWidget {
   const RestaurantScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<RestaurantScreen> createState() => _RestaurantScreenState();
+}
+
+class _RestaurantScreenState extends ConsumerState<RestaurantScreen> {
+  final ScrollController controller = ScrollController();
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(scrollListener);
+  }
+
+  void scrollListener() {
+    if (controller.offset > controller.position.maxScrollExtent - 300) {
+      ref.read(restaurantProvider.notifier).paginate(
+            fetchMore: true,
+          );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final data = ref.watch(restaurantProvider);
 
     if (data is CursorPaginationLoading) {
@@ -18,11 +39,18 @@ class RestaurantScreen extends ConsumerWidget {
       );
     }
 
+    if (data is CursorPaginationError) {
+      return Center(
+        child: Text(data.message),
+      );
+    }
+
     final cp = data as CursorPaginationModel;
 
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0),
       child: ListView.separated(
+        controller: controller,
         itemCount: cp.data.length,
         itemBuilder: (context, index) {
           final pItem = cp.data[index];
